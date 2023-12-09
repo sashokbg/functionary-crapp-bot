feb717faabe212fe7ab4c6fef3a30c22a37b5176
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-12-09 15:58:54 +0100

    added CRA App functions

---
+++ handlers.py
@@ -0,0 +1,138 @@
+import requests
+from datetime import date
+import json
+from dateutil import parser
+
+import curl
+
+
+def get_projects_for_user(user_json):
+    user = json.loads(user_json)
+
+    api_url = f"http://localhost:8080/v2/private/project?user={user['email']}"
+    response = requests.get(api_url)
+
+    print(response.json())
+
+    return response.json()
+
+
+def create_project(project_json):
+    project = json.loads(project_json)
+    project["date"] = date.today().strftime("%Y-%m-%d")
+    print(f"Creating a new project {project}")
+    api_url = f"http://localhost:8080/v2/private/project"
+    requests.post(api_url, json=project)
+    return True
+
+
+def get_all_employees():
+    api_url = f"http://localhost:8080/v2/private/employees"
+    response = requests.get(api_url)
+    return response.json()
+
+
+def add_employee(employee_json):
+    employee = json.loads(employee_json)
+    print(f"Creating a new employee {employee}")
+    api_url = f"http://localhost:8080/v2/private/employees"
+    response = requests.post(api_url, json=employee)
+    # curl.parse(response)
+    return True
+
+
+def get_all_projects():
+    api_url = f"http://localhost:8080/v2/private/project"
+    response = requests.get(api_url)
+    return response.json()
+
+
+def assign_project_to_employee(payload_json):
+    payload = json.loads(payload_json)
+    api_url = f"http://localhost:8080/v2/private/project/{payload['project_code']}"
+    response = requests.get(api_url)
+
+    response_project = response.json()
+
+    if response_project["employees"] is None:
+        response_project["employees"] = []
+
+    response_project["employees"].append(payload["email"])
+
+    api_url = "http://localhost:8080/v2/private/project"
+    response = requests.post(api_url, json=response_project)
+
+    return response.status_code
+
+
+def add_activity(activity_json):
+    activity = json.loads(activity_json)
+    api_url = "http://localhost:8080/v2/private/activity-report"
+
+    date_ = parser.parse(activity["date"])
+    to_post = {
+        "activities": [
+            {
+                "projectCode": activity["project_code"],
+                "activities": [
+                    {
+                        "title": activity["project_code"],
+                        "percentage": activity["work_time"],
+                        "type": "Project",
+                        "date": date_.strftime("%Y-%m-%d"),
+                        "project": {
+                            "code": activity["project_code"]
+                        }
+                    }
+                ]
+            }
+        ],
+        "employeeEmail": activity["email"],
+        "year": date_.strftime("%Y"),
+        "month": date_.strftime("%m")
+    }
+
+    response = requests.post(api_url, json=to_post)
+
+    return response.status_code
+
+
+def add_absence(activity_json):
+    absence = json.loads(activity_json)
+    api_url = "http://localhost:8080/v2/private/activity-report"
+
+    date_ = parser.parse(absence["date"])
+    to_post = {
+        "activities": [
+            {
+                "activities": [
+                    {
+                        "title": absence["reason"],
+                        "percentage": absence["absence_time"],
+                        "type": "Absence",
+                        "date": date_.strftime("%Y-%m-%d"),
+                        "reason": absence["reason"]
+                    }
+                ]
+            }
+        ],
+        "employeeEmail": absence["email"],
+        "year": date_.strftime("%Y"),
+        "month": date_.strftime("%m")
+    }
+
+    response = requests.post(api_url, json=to_post)
+    curl.parse(response)
+
+    return response.status_code
+
+
+def get_project_activities(params_json):
+    params = json.loads(params_json)
+
+    date_ = parser.parse(params["current_date"])
+    api_url = f"http://localhost:8080/v2/private/activity-report/{params['current_user']}/{date_.strftime('%Y')}/{date_.strftime('%m')}"
+
+    response = requests.get(api_url)
+
+    return response.json()
---
+++ run2.py
@@ -0,0 +1,108 @@
+from llama_cpp import Llama
+from functionary.prompt_template import get_prompt_template_from_tokenizer
+from transformers import AutoTokenizer
+import json
+import sys
+import pprint
+import handlers
+from datetime import date
+
+pp = pprint.PrettyPrinter(indent=2)
+
+json_text = open("functions.json", "r").read()
+
+functions = json.loads(json_text)
+
+# You can download gguf files from https://huggingface.co/meetkai/functionary-7b-v1.4-GGUF/tree/main
+llm = Llama(model_path="models/functionary-7b-v1.4.q4_0.gguf",
+            n_ctx=4096, n_gpu_layers=35, verbose=True)
+messages = [
+    {"role": "system", "content": "Environment is Python3"},
+    {"role": "system", "content": f'Current date is: {date.today()}'},
+    {"role": "system", "content": "Currently connected user is 'aleksandar@company.com'"},
+    {"role": "system", "content": "This program helps user fill in their monthly activity reports. Each activity "
+                                  "report is associated to a project that user has worked on. Activities can be "
+                                  "reported in increments of 25% per day. A single day cannot have more than 100% of "
+                                  "reported time"},
+    {"role": "user", "content": "Always ask me for missing function values"},
+    {"role": "user", "content": str(sys.argv[1])}
+]
+
+# Create tokenizer from HF.
+# We found that the tokenizer from llama_cpp is not compatible with tokenizer from HF that we trained
+# The reason might be we added new tokens to the original tokenizer
+# So we will use tokenizer from HuggingFace
+tokenizer = AutoTokenizer.from_pretrained(
+    "meetkai/functionary-7b-v1.4", legacy=True)
+
+# prompt_template will be used for creating the prompt
+prompt_template = get_prompt_template_from_tokenizer(tokenizer)
+
+
+def generate_message(messages, functions):
+    # Before inference, we will add an empty message with role=assistant
+    all_messages = list(messages) + [{"role": "assistant"}]
+
+    # Create the prompt to use for inference
+    prompt_str = prompt_template.get_prompt_from_messages(
+        all_messages, functions)
+    token_ids = tokenizer.encode(prompt_str)
+
+    gen_tokens = []
+    # Get list of stop_tokens
+    stop_token_ids = [tokenizer.encode(
+        token)[-1] for token in prompt_template.get_stop_tokens_for_generation()]
+    print("stop_token_ids: ", stop_token_ids)
+
+    # We use function generate (instead of __call__) so we can pass in list of token_ids
+    for token_id in llm.generate(token_ids, temp=0):
+        if token_id in stop_token_ids:
+            break
+        gen_tokens.append(token_id)
+
+    llm_output = tokenizer.decode(gen_tokens)
+    result = prompt_template.parse_assistant_response(llm_output)
+    return result
+
+
+# Create the prompt to use for inference
+while (True):
+    print("Sending prompt")
+    # pp.pprint(messages)
+
+    result = generate_message(messages, functions)
+
+    print(result)
+    messages.append(result)
+
+    if "function_call" in result:
+        func_name = result["function_call"]["name"]
+        func_param = result["function_call"]["arguments"]
+
+        print(f" ******** call function: {func_name}, arguments: {func_param}")
+
+        match func_name:
+            case 'get_all_projects':
+                result = handlers.get_all_projects()
+            case 'get_projects_for_user':
+                result = handlers.get_projects_for_user(func_param)
+            case 'create_project':
+                result = handlers.create_project(func_param)
+            case 'get_all_employees':
+                result = handlers.get_all_employees()
+            case 'add_employee':
+                result = handlers.add_employee(func_param)
+            case 'assign_project_to_employee':
+                result = handlers.assign_project_to_employee(func_param)
+            case 'add_activity':
+                result = handlers.add_activity(func_param)
+            case 'get_project_activities':
+                result = handlers.get_project_activities(func_param)
+            case 'add_absence':
+                result = handlers.add_absence(func_param)
+
+        messages.append(
+            {"role": "function", "name": func_name, "content": result})
+    else:
+        user_input = input("user: ")
+        messages.append({"role": "user", "content": user_input})
